ebfcf76eea1b82165d6afaad26b0a7d265a54de5
Jaswanth1702 <kjaswanthjashu445@gmail.com>
2024-10-06 18:10:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 let countEl = document.getElementById("count-el")
-// console.log(countEl)
 let saveEl = document.getElementById("save-el")
 let count = 0
 function increment(){
     count += 1
     countEl.textContent = count
-    // console.log(count)
 }
-
-// let save = 0
 function save(){
     let countStr = count + " - "
     saveEl.textContent += countStr
